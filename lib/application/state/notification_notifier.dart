1012a15e0871c66e58b00dea6fce27638cdc1283
taro <tarofess@gmail.com>
2025-02-19 16:10:17 +0900

    ブロックしているユーザーの通知が通知画面に表示されないよう修正

--- lib/application/state/notification_notifier.dart
+++ lib/application/state/notification_notifier.dart
@@ -3,6 +3,7 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:green_heart/application/di/notification_di.dart';
 import 'package:green_heart/application/state/auth_state_provider.dart';
 import 'package:green_heart/domain/type/notification.dart';
+import 'package:green_heart/application/di/block_di.dart';
 
 class NotificationNotifier extends AsyncNotifier<List<Notification>> {
   @override
@@ -14,8 +15,9 @@ class NotificationNotifier extends AsyncNotifier<List<Notification>> {
 
     final notifications =
         await ref.read(notificationGetUsecaseProvider).execute(uid);
+    final filteredNotifications = await filterByBlock(notifications);
 
-    return notifications;
+    return filteredNotifications;
   }
 
   Future<void> markAsRead(Notification notification) async {
@@ -31,6 +33,23 @@ class NotificationNotifier extends AsyncNotifier<List<Notification>> {
 
       state = AsyncValue.data(updatedNotifications);
     });
+  }
+
+  Future<List<Notification>> filterByBlock(
+    List<Notification> notifications,
+  ) async {
+    final uid = ref.watch(authStateProvider).value?.uid;
+    if (uid == null) {
+      throw Exception('ユーザー情報が取得できませんでした。再度お試しください。');
+    }
+
+    final myBlockList = await ref.read(blockGetUsecaseProvider).execute(uid);
+    final blockedUserIds = myBlockList.map((block) => block.targetUid).toSet();
+
+    return notifications
+        .where(
+            (notification) => !blockedUserIds.contains(notification.senderUid))
+        .toList();
   }
 }
 
